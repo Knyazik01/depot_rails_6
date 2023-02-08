ee6020e5a281bd8edb4c7fb3b354f013d7bc3bd3
Knyazik01 <Knyazik01>
2023-02-09 01:27:15 +0200

    Chapter 7. Task B: Validation and Unit Testing
    Playtime:
    - add length validation for title field (minimum 10)
    - set custom message for minimum validation (must have minimum 10 characters)

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
+  validates :title, allow_blank: true, :length => { :minimum => 10, message: 'must have minimum 10 characters' }
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
