35f9789fc322d17ce58e0e162d09e9ef1f9f1144
Knyazik01 <Knyazik01>
2023-02-11 00:51:21 +0200

    Chapter 8. Task C: Catalog Display
    + Playtime:
    1. Add last update (current time)
    2. Change number_to_currency format
    3. Add tests for products controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :set_opened_time
+
+  private
+  def set_opened_time
+    @page_opened_at = Time.now.strftime('%m/%d/%Y %H:%M:%S')
+  end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -9,6 +9,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get products_url
     assert_response :success
+    assert_select 'tbody tr', 3 # 3 element
+    assert_select '.description h1', 'Programming Ruby 1.9' # last element title
+    # products items
+    assert_select 'tbody tr' do |items|
+      items.each do |item|
+        assert_select item, '.actions li', 3 # option element
+      end
+    end
   end
 
   test "should get new" do
